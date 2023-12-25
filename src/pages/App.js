1cb46db6bcbe15d071ef2d17c2bd7982085ea27c
sousax <eduardomssousa@hotmail.com>
2023-12-25 15:30:55 -0300

    Criação do itemList com os dados

--- src/pages/App.js
+++ src/pages/App.js
@@ -9,6 +9,14 @@ function App() {
       <div className="info">
         <h4>Informações</h4>
       </div>
+      <div className="info-cnpj">
+        <h3>Razão social: Eduardo</h3>
+        <h4>Nome fantasia: Emss Consultoria LTDA </h4>
+        <ul>
+          <li>CNPJ</li>
+          <li></li>
+        </ul>
+      </div>
     </div>
   );
 }
